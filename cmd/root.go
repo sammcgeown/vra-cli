d37eb9a45e3764b1f60e687021820959be47a9f8
Sam McGeown <smcgeown@vmware.com>
2021-07-06 13:26:11 +0100

    Add timestamp to logging

--- cmd/root.go
+++ cmd/root.go
@@ -80,6 +80,7 @@ func init() {
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
 	// Debug logging
+	log.SetFormatter(&log.TextFormatter{TimestampFormat: "2006-01-02 15:04:05", FullTimestamp: true})
 	if debug {
 		log.SetLevel(log.DebugLevel)
 		log.Debugln("Debug logging enabled")
