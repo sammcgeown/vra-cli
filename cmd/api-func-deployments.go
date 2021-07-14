23708d74672976e809862792e669fae83e4b9e95
Sam McGeown <smcgeown@vmware.com>
2021-07-14 16:21:35 +0100

    Add deployments

---
+++ cmd/api-func-deployments.go
@@ -0,0 +1,61 @@
+/*
+Package cmd Copyright 2021 VMware, Inc.
+SPDX-License-Identifier: BSD-2-Clause
+*/
+package cmd
+
+import (
+	"crypto/tls"
+	"errors"
+
+	"github.com/go-resty/resty/v2"
+	"github.com/mitchellh/mapstructure"
+	log "github.com/sirupsen/logrus"
+)
+
+func getDeployments(id string) ([]*Deployment, error) {
+	var arrResults []*Deployment
+	client := resty.New()
+
+	if id != "" {
+		queryResponse, _ := client.SetTLSClientConfig(&tls.Config{InsecureSkipVerify: ignoreCert}).R().
+			SetQueryParams(qParams).
+			SetHeader("Accept", "application/json").
+			SetResult(&Deployment{}).
+			SetAuthToken(targetConfig.accesstoken).
+			SetError(&CodeStreamException{}).
+			Get("https://" + targetConfig.server + "/deployment/api/deployments/" + id)
+
+		log.Debugln(queryResponse.Request.RawRequest.URL)
+		// log.Debugln(queryResponse.String())
+
+		if queryResponse.IsError() {
+			return nil, errors.New(queryResponse.Error().(*CodeStreamException).Message)
+		}
+
+		arrResults = append(arrResults, queryResponse.Result().(*Deployment))
+	} else {
+		queryResponse, _ := client.SetTLSClientConfig(&tls.Config{InsecureSkipVerify: ignoreCert}).R().
+			SetQueryParams(qParams).
+			SetHeader("Accept", "application/json").
+			SetResult(&contentsList{}).
+			SetAuthToken(targetConfig.accesstoken).
+			SetError(&CodeStreamException{}).
+			Get("https://" + targetConfig.server + "/deployment/api/deployments")
+
+		log.Debugln(queryResponse.Request.RawRequest.URL)
+		// log.Debugln(queryResponse.String())
+
+		if queryResponse.IsError() {
+			return nil, errors.New(queryResponse.Error().(*CodeStreamException).Message)
+		}
+
+		for _, value := range queryResponse.Result().(*contentsList).Content {
+			c := Deployment{}
+			mapstructure.Decode(value, &c)
+			arrResults = append(arrResults, &c)
+		}
+
+	}
+	return arrResults, nil
+}
---
+++ cmd/deployment.go
@@ -0,0 +1,60 @@
+/*
+Package cmd Copyright 2021 VMware, Inc.
+SPDX-License-Identifier: BSD-2-Clause
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/olekukonko/tablewriter"
+	log "github.com/sirupsen/logrus"
+
+	"github.com/spf13/cobra"
+)
+
+// getDeploymentCmd represents the variable command
+var getDeploymentCmd = &cobra.Command{
+	Use:   "deployment",
+	Short: "Get Deployments",
+	Long:  `Get Deployments`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if err := ensureTargetConnection(); err != nil {
+			log.Fatalln(err)
+		}
+
+		response, err := getDeployments(id)
+		if err != nil {
+			log.Fatalln("Unable to get Deployments: ", err)
+		}
+		var resultCount = len(response)
+		if resultCount == 0 {
+			// No results
+			log.Warnln("No results found")
+		} else if resultCount == 1 {
+			// Print the single result
+			if exportPath != "" {
+				exportVariable(response[0], exportPath)
+			}
+			PrettyPrint(response[0])
+		} else {
+			// Print result table
+			table := tablewriter.NewWriter(os.Stdout)
+			table.SetHeader([]string{"Id", "Name", "Project", "Description"})
+			for _, c := range response {
+				table.Append([]string{c.Id, c.Name, c.ProjectId, c.Description})
+			}
+			table.Render()
+		}
+	},
+}
+
+func init() {
+	// Get Variable
+	getCmd.AddCommand(getDeploymentCmd)
+	getDeploymentCmd.Flags().StringVarP(&name, "name", "n", "", "List variable with name")
+	getDeploymentCmd.Flags().StringVarP(&project, "project", "p", "", "List variables in project")
+	getDeploymentCmd.Flags().StringVarP(&id, "id", "i", "", "List variables by id")
+	getDeploymentCmd.Flags().StringVarP(&exportPath, "exportPath", "", "", "Path to export objects - relative or absolute location")
+
+}
